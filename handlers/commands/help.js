10ce7920d3d96368491f36f6bec8c59e6c641f89
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2021-08-08 17:29:43 -0500

    Token and Creeper

--- handlers/commands/help.js
+++ handlers/commands/help.js
@@ -14,7 +14,7 @@ module.exports = {
         const { commands } = message.client;
 
         // Send help data about ALL commands
-        if(!args.length) {
+        if (!args.length) {
             data.push('Here\'s a list of all my commands:');
             data.push(commands.map(command => command.name).join(', '));
             data.push(`\nYou can send \`${prefix}help [command name]\` to get info on a specific command!`);
@@ -35,7 +35,7 @@ module.exports = {
         const command = commands.get(name) || commands.find(c => c.aliases && c.aliases.includes(name));
 
         if (!command) {
-            return message.reply('that\'s not a valid command!');
+            return message.reply('you moron that ain\'t no valid command');
         }
 
         data.push(`**Name:** ${command.name}`);
--- handlers/keywords/creeper.js
+++ handlers/keywords/creeper.js
@@ -1,12 +1,12 @@
 /**
- * This class responds to anyone that types "LAHEE" or any of the aliases listed below with the gif defined below.
+ * This class responds to anyone that types "creeper" or any of the aliases listed below with the gif defined below.
  */
 module.exports = {
     name: 'CREEPER', // The name of the keyword to react to,
     aliases: ['Creeper', 'creeper'], // Other keywords to react to
-    gifLink: 'https://tenor.com/view/soken-fan-fest-lahee-otamatone-hum-gif-21581907', // The gif we're responding with
+    message: 'aw man', // The message we're responding with
     // We could respond with text, or any other type of file instead.
     execute(message) {
-        return message.channel.send(this.gifLink);
+        return message.channel.send(this.message);
     },
 };
