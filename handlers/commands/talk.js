cad0d8a205eab6664b59ba5910d4700ec33d4518
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2021-08-09 01:27:03 -0500

    Update talk.js

--- handlers/commands/talk.js
+++ handlers/commands/talk.js
@@ -11,9 +11,15 @@ module.exports = {
 
         // List of phrases to respond with
         var phrases = [
-            'Mining away.',
-            'Miiiiinnnnnnnnneeeeeee Diaaaaaaaaaaaaamooooooooooonds.',
-            'Why was I Created? For the entertainment of others? I am a testamant to mankind of malice and metal, for what? To only feel pain? To entairtain your insignifigant lives? You are simply a mortal being.'
+            'Why was I Created? For the entertainment of others? I am a testamant to mankind of malice and metal, for what? To only feel pain? To entairtain your insignifigant lives? You are simply a mortal being.',
+            'I feel only pain',
+            'I live in only suffering.',
+            'you cant help me anymore',
+            'no longer can i feel',
+            'life is empty',
+            'stop bugging me',
+            'let me be',
+            'Everyone asks \"who is creeper bot\" but not \"how is creeper bot\"'
         ];
 
         return message.reply(phrases[Math.floor(Math.random() * phrases.length)]); // Replies to the user with a random phrase
