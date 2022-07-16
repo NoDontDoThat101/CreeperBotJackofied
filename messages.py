39b37cb0c18351159fca61b64b44fa46bbbe83a1
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2022-07-16 14:13:19 -0500

    Moved messages
    Moved messages to Messages.py and updated variables

---
+++ messages.py
@@ -0,0 +1,19 @@
+banMessages = [
+    "Stop playing league of legends",
+    "Take a shower",
+    "Among us??? Sussy??? Imposter?????"
+]
+
+games = [
+    "leauge of legends",
+    "genshin impact",
+    "destiny 2",
+    "overwatch",
+]
+
+status = [
+    "gaming",
+    "Just chilling",
+    "Having a good time",
+    "Banning LOL players",
+]
