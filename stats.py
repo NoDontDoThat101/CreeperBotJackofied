2354dc444dc32a3d83eff4155f880b05c32a0a26
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2023-04-21 15:54:23 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,23 +1,12 @@
 from pathlib import Path
 import pickle
-f = None       
-data = {
-    'defualt':1,
-    'save':2,  
-    'balls':3  
-}
-
+f = None
 def save(data, close=False):
-        path = Path(__file__).parent
-        with path.open("data.pickle", "wb") as f:
+        with open('D:\Python Scripts\CreeperBot\data.pickle', "wb") as f:
             pickle.dump(data, f)
             f.close()
-
 def load():
-    path = Path(__file__).parent
-    with path.open("data.pickle", "rb") as f:
-        d = pickle.load(f)
-        f.close()
-        return d
-
-save(data)
+    f = open('D:\Python Scripts\CreeperBot\data.pickle', "rb")
+    d = pickle.load(f)
+    f.close()
+    return d
