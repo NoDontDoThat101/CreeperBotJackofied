8beaad8bc5b23dfd22ad4e6c7f7e9be32238d080
VoidIsNoLongerHere <VoidIsNoLongerHere@users.noreply.github.com>
2023-05-22 15:55:57 -0500

    Update messages.py
    Removed deprecated variables

--- messages.py
+++ messages.py
@@ -1,12 +1,5 @@
 import random
 
-games = [
-    "leauge of legends",
-    "genshin impact",
-    "destiny 2",
-    "overwatch",
-]
-
 playing = [
     "Sex with Stalin",
     f"Furry Hentai {random.randint(1,100)}",
@@ -22,13 +15,6 @@ watching = [
     "Gay Furry Porn",
     "How To Psycologically Torture Your Friends: Part " + str(random.randint(1,100000)),
     'Your messages' 
-]
-custom = [
-    "I see you",
-    "Will you marry me?",
-    "Sussy Baka",
-    "I Love You ",
-    "@whistle, will you marry me?"
 ]
 
 rareResponses = [
@@ -51,6 +37,4 @@ def status(type):
     elif type == 'playing':
     #Chooses from playing List
        return random.choice(playing)
-    elif type == 'custom':
-        return random.choice(custom)
     
